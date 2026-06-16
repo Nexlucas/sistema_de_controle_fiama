9d9c8000a3747b51cb775f51ffb2d97d35ac8b61
Nexlucas <luka.santos1105@gmail.com>
2026-06-16 11:04:26 -0300

    mudanças

--- st_controle/src/main/java/com/biolab/controle/repositories/MaterialRepository.java
+++ st_controle/src/main/java/com/biolab/controle/repositories/MaterialRepository.java
@@ -3,9 +3,14 @@ package com.biolab.controle.repositories;
 import com.biolab.controle.controllers.MaterialControllers;
 import com.biolab.controle.entities.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MaterialRepository extends JpaRepository<Material, Long> {
     List<Material> findBycategoria(String categoria);
+
+    @Query(value = "SELECT categoria,SUM(quantidade * valor_unitario) as valor_total FROM material GROUP BY categoria ORDER BY valor_total DESC", nativeQuery = true)
+    List<String> listarValorTotal();
+
 }
